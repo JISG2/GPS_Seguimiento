7b9d6aae4278bb1dd5d929dc9bb7ef146a387673
Israel Gutierrez <israel_gutierrez3@hotmail.com>
2019-03-05 22:28:06 -0600

    Background Geolocation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,16 +3,20 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import {BackgroundGeolocation,BackgroundGeolocationConfig,BackgroundGeolocationResponse,BackgroundGeolocationEvents} from '@ionic-native/background-geolocation/ngx'
 
+declare var window;
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
+  arr: any;
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private backgroundGeolocation: BackgroundGeolocation
   ) {
     this.initializeApp();
   }
@@ -21,6 +25,30 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+      
+      const config: BackgroundGeolocationConfig={
+        desiredAccuracy:10,
+        stationaryRadius:20,
+        distanceFilter:30,
+        debug: true,
+        stopOnTerminate:false,
+
+      };
+      this.backgroundGeolocation.configure(config).then(()=>{
+        this.backgroundGeolocation.on(BackgroundGeolocationEvents.location).subscribe(
+          (location:BackgroundGeolocationResponse)=>{
+            var locationstr = localStorage.getItem('location');
+            if(locationstr == null){
+              this.arr.push(location);
+            }
+            else{
+              var locationarr = JSON.parse(locationstr)
+            }
+            localStorage.setItem("location",JSON.stringify(this.arr));
+          }
+        )
+      })
+      window.app = this;
     });
   }
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
 
+declare var window;
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  locations:any;
+
+  constructor(){
+    this.locations = [];
+
+  }
+
+  StartBackgroundTracking(){
+    window.app.backgorundGeolocation.start();
+  }
+
+  StopBackgroundGeolocation(){
+    window.app.backgorundGeolocation.stop();
+  }
+
+  Geolocations(){
+    this.locations = (JSON.parse(localStorage.getItem("location"))== null)?[]:JSON.parse(localStorage.getItem("location"));
+  }
+
+  ClearLocations(){
+    localStorage.removeItem("location");
+  }
 
 }
